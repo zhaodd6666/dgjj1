f1f218cba1a47967317ee4737df321e0a82c4f27
zhaodd6666 <2175866818@qq.com>
2020-05-28 11:40:55 +0800

    新增

--- src/dgjj1/DGJJ1.java
+++ src/dgjj1/DGJJ1.java
@@ -4,6 +4,10 @@ public class DGJJ1 {
 
 	public static void main(String[] args) {
 		System.out.println("¸ÇÊÀÓ¢ÐÛ");
+		System.out.println("¶À");
+		System.out.println("¹Â");
+		System.out.println("¾Å");
+		System.out.println("½£");
 
 	}
 
