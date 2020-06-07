58ab5f6e91730c12b700030e6b91bb843c29c709
zhaodd6666 <65893091+zhaodd6666@users.noreply.github.com>
2020-06-07 16:05:46 +0800

    Update DGJJ1.java

--- src/dgjj1/DGJJ1.java
+++ src/dgjj1/DGJJ1.java
@@ -4,8 +4,8 @@ public class DGJJ1 {
 
 	public static void main(String[] args) {
 
-		System.out.println("����Ӣ��");
-		System.out.println("��");
+		System.out.println("gitnub的操作步骤：");
+		System.out.println("将远程的项目从github,pull下来，然后重新add,commit，post。");
 		System.out.println("��");
 		System.out.println("��");
 		System.out.println("��");
